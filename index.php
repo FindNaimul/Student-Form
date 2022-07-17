4e84107ed6e31d1b8bcebce66eb1d21f0fe64117
Naimul hasan Nahid <108421244+FindNaimul@users.noreply.github.com>
2022-07-18 00:11:56 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,143 @@
+<?php
+        $crn="CRN";
+        $code="Subject Code";
+        $number="Course Number";
+        $name="Name";
+        $term="Term";
+        $id= "Instructor ID";
+
+?>
+
+<table border="1" cellspacing="3" cellpadding="5" rules="all">
+    <!-- 1st row -->
+    <tr align="center">
+        <td>
+            <?php echo $crn; ?>
+        </td>
+        <td>
+            <?php echo $code; ?>
+        </td>
+        <td>
+            <?php echo $number; ?>
+        </td>
+        <td>
+            <?php echo $name; ?>
+        </td>
+        <td>
+            <?php echo $term; ?>
+        </td>
+        <td>
+            <?php echo $id; ?>
+        </td>
+    </tr>
+
+<!-- 2nd  row -->
+            <tr align="center">
+                <td>
+                    <?php echo "49304"; ?>
+                </td>
+                <td>
+                    <?php echo "CMPT"; ?>
+                </td>
+                <td>
+                    <?php echo "260"; ?>
+                </td>
+                <td>
+                    <?php echo "Mathmatical logic and Computing"; ?>
+                </td>
+                <td>
+                    <?php echo "2014"; ?>
+                </td>
+                <td>
+                    <?php echo "2"; ?>
+                </td>
+            </tr>
+
+<!-- 3rd -->
+
+<tr align="center">
+                <td>
+                    <?php echo "20782"; ?>
+                </td>
+                <td>
+                    <?php echo "CMPT"; ?>
+                </td>
+                <td>
+                    <?php echo "110"; ?>
+                </td>
+                <td>
+                    <?php echo "Introductory Computer Science 1"; ?>
+                </td>
+                <td>
+                    <?php echo "2013"; ?>
+                </td>
+                <td>
+                    <?php echo "1"; ?>
+                </td>
+            </tr>
+
+            <tr align="center">
+                <td>
+                    <?php echo "98028"; ?>
+                </td>
+                <td>
+                    <?php echo "CMPT"; ?>
+                </td>
+                <td>
+                    <?php echo "115"; ?>
+                </td>
+                <td>
+                    <?php echo "Introductory Compuer Science 2"; ?>
+                </td>
+                <td>
+                    <?php echo "2013"; ?>
+                </td>
+                <td>
+                    <?php echo "4"; ?>
+                </td>
+            </tr>
+
+
+            <tr align="center">
+                <td>
+                    <?php echo "32894"; ?>
+                </td>
+                <td>
+                    <?php echo "MATH"; ?>
+                </td>
+                <td>
+                    <?php echo "110"; ?>
+                </td>
+                <td>
+                    <?php echo "Calculus"; ?>
+                </td>
+                <td>
+                    <?php echo "2013"; ?>
+                </td>
+                <td>
+                    <?php echo "5"; ?>
+                </td>
+            </tr>
+
+
+            <tr align="center">
+                <td>
+                    <?php echo "24684"; ?>
+                </td>
+                <td>
+                    <?php echo "EE"; ?>
+                </td>
+                <td>
+                    <?php echo "232"; ?>
+                </td>
+                <td>
+                    <?php echo "Digital Electronics"; ?>
+                </td>
+                <td>
+                    <?php echo "2014"; ?>
+                </td>
+                <td>
+                    <?php echo "6"; ?>
+                </td>
+            </tr>
+</table>
